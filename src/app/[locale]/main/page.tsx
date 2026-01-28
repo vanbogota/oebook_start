3505e950c5eb6bcc7bb24b15326132b49c6e33c7
Anna <cameaann@gmail.com>
2026-01-28 10:38:51 +0200

    added two tabs, added icons to the tabs

--- src/app/[locale]/main/page.tsx
+++ src/app/[locale]/main/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import {
   Tabs,
   TabsContent,
@@ -10,39 +10,51 @@ import {
 import { Header } from "@/components/Header";
 import PrintRequestForm from "@/components/PrintRequestForm";
 import BookSearch from "@/components/BookSearch";
-import { Printer, Search as SearchIcon } from "lucide-react";
+import { PrinterIcon, Search as SearchIcon, ListIcon,  ScanIcon, BookOpenIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 import WaitingListForm from "@/components/WaitingListForm";
+import { TabContext, TabType } from "@/contexts/MainTabContext";
+import PrintBookPage from "../print-book/page";
+import ReadBookPage from "../read-book/page";
+import ScanBookPage from "../scan-book/page";
 
 export default function MainPage() {
   const t = useTranslations("MainPage");
-  const [activeTab, setActiveTab] = useState<string>("waiting");
+ const { activeTab, changeTab } = useContext(TabContext);
 
-  const handleTabChange = (value: string) => {
-    setActiveTab(value);
+  const handleTabChange = (value: TabType) => {
+    changeTab(value);
   };
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-background to-secondary/30">
       <Header />
-      <div className="container mx-auto px-4 py-12 max-w-6xl">
+      <div className="container mx-auto px-2 md:px-10 lg:px-40 py-12 max-w-[6xl]">
         <Tabs
           value={activeTab}
           onValueChange={handleTabChange}
           className="w-full"
         >
-          <TabsList className="grid w-full max-w-md mx-auto grid-cols-3 mb-8">
+          <TabsList className="grid w-full max-w-3xl mx-auto grid-cols-5 mb-8">
             <TabsTrigger value="waiting" className="flex items-center gap-2">
-              <Printer className="w-4 h-4" />
-              Join Waiting List
+              <ListIcon className="w-4 h-4" />
+              <span className="hidden md:inline">{t("waiting-list")}</span>
             </TabsTrigger>
             <TabsTrigger value="search" className="flex items-center gap-2">
               <SearchIcon className="w-4 h-4" />
-              {t("search-button")}
+              <span className="hidden md:inline">{t("search-book")}</span>
+            </TabsTrigger>
+            <TabsTrigger value="scan" className="flex items-center gap-2">
+              <ScanIcon className="w-4 h-4" />
+              <span className="hidden md:inline">{t("scan-book")}</span>
+            </TabsTrigger>
+            <TabsTrigger value="read" className="flex items-center gap-2">
+              <BookOpenIcon className="w-4 h-4" />
+              <span className="hidden md:inline">{t("read-book")}</span>
             </TabsTrigger>
             <TabsTrigger value="print" className="flex items-center gap-2">
-              <SearchIcon className="w-4 h-4" />
-              Order Paper Book
+              <PrinterIcon className="w-4 h-4" />
+             <span className="hidden md:inline">{t("print-book")}</span>
             </TabsTrigger>
           </TabsList>
 
@@ -52,10 +64,14 @@ export default function MainPage() {
           <TabsContent value="search">
             <BookSearch />
           </TabsContent>
+          <TabsContent value="scan">
+            <ScanBookPage />
+          </TabsContent>
+          <TabsContent value="read">
+            <ReadBookPage />
+          </TabsContent>
           <TabsContent value="print">
-             <div className="text-center">{t.rich("page-under-construction", {
-               important: (chunks) => <b className="underline" onClick={() => handleTabChange("waiting")}>{chunks}</b>,
-             })}</div>
+             <PrintBookPage />
             {/* <PrintRequestForm /> */}
           </TabsContent>
         </Tabs>
